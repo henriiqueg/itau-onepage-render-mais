e4732460453536b7045fdb253cd684be30060994
Henrique Gonçalves <henriquesg09@hotmail.com>
2021-02-17 18:05:31 -0300

    Develop: adjust responsivity

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -3,10 +3,10 @@ import vendaMaisLogo from 'assets/img/logo-venda-mais.png';
 const Page = ({ children, ...props }) => (
   <div
     className="min-h-screen relative flex flex-col gap-8 items-center
-    justify-center max-w-screen-md pb-8 px-6 w-screen pt-32 md:pt-0"
+    justify-center max-w-screen-md py-8 px-6 w-screen md:pt-0"
   >
     <img
-      className="w-full absolute top-7 left-1/2 transform -translate-x-2/4 "
+      className="w-full absolute top-7 left-1/2 transform -translate-x-2/4 hidden lg:block "
       style={{ maxWidth: '150px' }}
       src={vendaMaisLogo}
       alt="Venda Mais Logo"
--- src/components/Patterns/Modals/ModalWrong.jsx
+++ src/components/Patterns/Modals/ModalWrong.jsx
@@ -4,13 +4,13 @@ import Description from 'components/Patterns/Description';
 import icon from 'assets/icons/wrong.png';
 
 const ModalWrong = ({ children, onClick }) => (
-  <div className="absolute w-screen h-screen bg-secondary bg-opacity-50 flex items-center justify-center">
+  <div className="fixed w-full h-full bg-secondary bg-opacity-50 flex items-center justify-center top-0">
     <div
       className="w-11/12 bg-modal flex flex-col items-center justify-center"
       style={{ maxWidth: 450 }}
     >
       <div className="bg-modal w-full h-1/2 flex flex-col items-center justify-center p-8">
-        <img src={icon} alt="" />
+        <img src={icon} alt="" className="w-2/5" />
       </div>
       <div className="bg-secondary w-full h-1/2 flex flex-col items-center justify-center p-8 gap-4">
         <Description>{children}</Description>
